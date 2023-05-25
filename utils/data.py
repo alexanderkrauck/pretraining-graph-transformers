37d06fee1dd34e038b9d83b9542460d8f1d46a63
Alexander Krauck <alexander.krauck@gmail.com>
2023-05-25 18:27:30 +0200

    Removed deprecated function

--- utils/data.py
+++ utils/data.py
@@ -98,53 +98,6 @@ e_map = {
 }
 
 
-# TODO: unify the features. node_feat and edge_attr should be the same as when loading sdf files.
-def pyg_to_arrow(pyg_dataset: Dataset, to_disk_location: str = None):
-    """
-    Converts a PyG dataset to a Hugging Face dataset.
-
-    Adds a column "num_nodes" to the Hugging Face dataset, which contains the number of nodes for each graph.
-    Also renames the column "x" to "node_feat" to comply with huggingface standards.
-
-    Args
-    ----
-    pyg_dataset: torch_geometric.data.dataset.Dataset
-        PyG dataset to convert.
-    to_disk_location: str
-        Path to save the dataset to (the directory)."""
-
-    # Prepare data for PyArrow Table
-    data_for_arrow = {}
-
-    # Iterate over all keys in the first data object in the PyG dataset
-    keys = pyg_dataset[0].keys
-    for key in keys:
-        data_for_arrow[key] = []
-    data_for_arrow["num_nodes"] = []
-
-    for graph in tqdm(pyg_dataset):
-        for key in keys:
-            if key == "x":
-                data_for_arrow["num_nodes"].append(len(graph[key]))
-            feature = graph[key]
-            if isinstance(feature, torch.Tensor):
-                data_for_arrow[key].append(feature.tolist())
-            else:
-                data_for_arrow[key].append(feature)
-
-    # Convert Dict to Hugging Face Dataset
-    hf_dataset = Dataset.from_dict(data_for_arrow)
-    hf_dataset = hf_dataset.rename_column("x", "node_feat")
-
-    print(f"Resulting Dataset: {hf_dataset}")
-
-    if to_disk_location is not None:
-        hf_dataset.save_to_disk(to_disk_location)
-        print(f"Saved dataset to {to_disk_location}.")
-
-    return hf_dataset
-
-
 def load_smiles_dataset(dataset_location: str, smiles_column: Optional[int] = None):
     """
     Loads a dataset from a csv file.
